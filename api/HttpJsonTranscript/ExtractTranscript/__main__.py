d7a3ee37eec719b21de0d3c10d4fc39a13ec6573
Michael Saunby <mike@saunby.net>
2020-09-10 22:12:15 +0000

    extractTranscript  from ffmpeg-container

---
+++ api/HttpJsonTranscript/ExtractTranscript/__main__.py
@@ -0,0 +1,9 @@
+from __init__ import insights_to_srt
+
+result = []
+outf = open("transcript.srt", "wt")
+with open('recording.ogg_insights.json', 'r', encoding='utf-8-sig') as f:
+    insights_to_srt(f, result)
+    outf.writelines(result)
+    outf.close()
+    print("Done.")
---
+++ api/HttpJsonTranscript/__init__.py
@@ -0,0 +1,18 @@
+import logging
+
+import azure.functions as func
+
+import tempfile
+
+import json
+
+from . import ExtractTranscript
+
+def main(req: func.HttpRequest, jsonBlob: func.InputStream, outputBlob: func.Out[str]) -> func.HttpResponse:
+    
+    insightsData = json.loads(jsonBlob.read(-1))
+    transcript = []
+    ExtractTranscript.insights_to_vtt(insightsData,transcript)
+    outputBlob.set(''.join(transcript))
+
+    return func.HttpResponse(f"Done",status_code=200)
