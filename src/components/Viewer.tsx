6562cae2539ff305f67d054a4a1e36cc5b29bf95
Michael Saunby <mike@saunby.net>
2020-09-12 22:02:32 +0000

    transcript loads to data url

--- src/components/Viewer.tsx
+++ src/components/Viewer.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 // See https://developer.mozilla.org/en-US/docs/Web/Guide/Audio_and_video_delivery/Adding_captions_and_subtitles_to_HTML5_video
 // 
 function Viewer() {
 
-  let { slug } = useParams();
+  let { project } = useParams();
 
-  let project = slug;
+  //let transcript = "https://saunby.blob.core.windows.net/recordings/" + project + ".vtt";
+  //let transcript = "https://saunby.blob.core.windows.net/recordings/recording.vtt";
 
+  const [transcript, setTranscript] = useState('data:text/plain;base64,');
+  
   let data = {
     "Insights": {
       "FileName": project + ".ogg_insights.json"
@@ -19,12 +23,17 @@ function Viewer() {
   };
 
   async function createTranscriptCall(): Promise<string> {
-    console.info("fetching",JSON.stringify(data));
+    console.info("fetching", JSON.stringify(data));
     let response = await fetch('/api/HttpJsonTranscript', {
       method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json',
       },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
       body: JSON.stringify(data),
     });
     if (!response.ok) {
@@ -33,8 +42,10 @@ function Viewer() {
     return await response.text();
   }
 
-  let createTranscript = () =>{
-    createTranscriptCall().then((msg)=>{console.info(msg)}).catch((err)=>{console.warn(err);})
+  let createTranscript = () => {
+    createTranscriptCall().then((msg) => { console.info(msg);
+      setTranscript('data:text/plain;base64,'+msg)
+     }).catch((err) => { console.warn(err); })
   }
 
   return (
@@ -43,18 +54,17 @@ function Viewer() {
         <div className="row align-items-center my-5">
           <div className="col-lg-9">
             <p>
-              {slug}
+              {project}
             </p>
             <p>
-        <button id="transcript" onClick={createTranscript}>Create transcript</button>
-        </p>
+              <button id="transcript" onClick={createTranscript}>Create transcript</button>
+            </p>
             <p>
               <video controls id="video" preload="metadata" crossOrigin="*" autoPlay>
                 <source src={"https://saunby.blob.core.windows.net/recordings/" + project + ".webm"}
                   type="video/webm" />
-                <track label="English" kind="subtitles" srcLang="en" src={"https://saunby.blob.core.windows.net/recordings/" + project + ".vtt"} default>
+                <track label="English" kind="subtitles" srcLang="en" src={transcript} default>
                 </track>
-
               </video>
             </p>
           </div>
